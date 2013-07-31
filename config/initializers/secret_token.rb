9959974c71305ce2d3e65c9ae557aa2e06c32c1e
amija khan <achan@ubuntu.(none)>
2013-07-30 17:40:40 -0700

    frist!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Frakedup::Application.config.secret_key_base = '2381592d6731ed27caa74d175a535de799648c249d2ff1a0be562b3f22154276df82f12ddcb2bd9c84e328d5d9b1ba9fbeedb1ba6a67c291b11f124ae3466523'
